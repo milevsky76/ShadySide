31776008cbe5936fa4c118ad77b8586d7fa254ac
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-05-01 23:23:04 +0300

    Преобразование RGB в Hex

--- js/main.js
+++ js/main.js
@@ -11,3 +11,22 @@ ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
 //Цвет 1
 ctx.fillStyle = 'rgba( 20 , 100 , 150, 0.5)';
 ctx.fillRect(canvas.clientWidth / 2, 0, canvas.clientWidth - canvas.clientWidth / 2, canvas.clientHeight / 2);
+
+//Преобразование RGB в Hex
+function rgbToHex(R, G, B) {
+    return toHex(R) + toHex(G) + toHex(B);
+}
+
+//Нахождение hex, принимает один из цветовых каналов rgb
+function toHex(n) {
+    n = parseInt(n, 10);
+
+    if (isNaN(n)) {
+        return "00";
+    }
+
+    n = Math.max(0, Math.min(n, 255));
+
+    return "0123456789ABCDEF".charAt((n - n % 16) / 16) + "0123456789ABCDEF".charAt(n % 16);
+}
+
