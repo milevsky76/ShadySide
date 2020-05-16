7894d175280c1bbf8c9a9e7c2c1189807efe4678
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-05-16 23:40:57 +0300

    Генерация localstorage 1 к 10
    Генерация на localstorage, один файл за 10 секунд

--- js/main.js
+++ js/main.js
@@ -4,9 +4,96 @@ let canvas = document.getElementById('canvas');
 //Получение контекста рисования на холсте
 let ctx = canvas.getContext('2d');
 
+let fillColor = '0,0,0';
 //Заливка холста
-ctx.fillStyle = 'rgba( 255 , 255, 255, 1)';
-ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+// ctx.fillStyle = 'rgba( ' + fillColor + ',1)';
+// ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+let mass = [];
+
+let fileBody = {};
+// let alpha;
+let imageData2;
+//============================>
+// const start = new Date().getTime();
+let usR = localStorage.getItem('usR');
+let usG = 0;
+let usB = 0;
+let ff = false;
+let a = localStorage.getItem('a');
+
+//альфа канал 1-99
+if (!a) {
+    localStorage.setItem('a', 1);
+    a = localStorage.getItem('a');
+}
+
+//under side RED
+if (!usR) {
+    localStorage.setItem('usR', 1);
+    usR = localStorage.getItem('usR');
+}
+
+//Основной цикл
+if (a < 100) {
+    pick();
+    location.href = location.href;
+}
+
+//Запаковка в файл
+if (a == 100) {
+    lsToFile();
+    download(`${usR},0,0.txt`, `'${JSON.stringify(fileBody)}'`);
+    a++;
+    localStorage.setItem('a', a);
+    ff = true;
+
+    console.log('Конец!');
+}
+
+//Увеличение under side RED на 1
+if (a == 101 && ff) {
+    setTimeout(() => alert('Привет'), 1000);
+    usR++;
+    localStorage.clear();
+    localStorage.setItem('usR', usR);
+}
+
+// const end = new Date().getTime();
+// console.log(`SecondWay: ${((end - start) / 1000)}s`);
+//============================>
+
+function pick() {
+    // for (let usA = 96; usA < 99; usA++) {
+    // if ((usA + '').length == 1) {
+    //     alpha = '0.0' + usA - 0;
+    // }
+    // if ((usA + '').length == 2) {
+    //     alpha = '0.' + usA - 0;
+    // }
+
+    ctx.fillStyle = 'rgba( ' + fillColor + ',1)';
+    ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+    ctx.fillStyle = `rgba( ${usR} ${usG} ${usB} / ${a}%)`;
+    ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+    imageData2 = ctx.getImageData(0, 0, 1, 1).data;
+
+    localStorage.setItem(a, `${imageData2[0]},${imageData2[1]},${imageData2[2]}`);
+
+    a++;
+
+    localStorage.setItem('a', a);
+}
+
+function lsToFile() {
+    for (let i = 1; i < 100; i++) {
+        mass.push(localStorage.getItem(i));
+
+        fileBody[`${usR},${usG},${usB}`] = mass;
+    }
+}
 
 function download(filename, text) {
     let fileLink = document.createElement('a');
@@ -20,9 +107,6 @@ function download(filename, text) {
 
     document.body.removeChild(fileLink);
 }
-//Цвет 1
-ctx.fillStyle = 'rgba( 20 , 100 , 150, 0.5)';
-ctx.fillRect(canvas.clientWidth / 2, 0, canvas.clientWidth - canvas.clientWidth / 2, canvas.clientHeight / 2);
 
 //Преобразование RGB в Hex
 function rgbToHex(R, G, B) {
